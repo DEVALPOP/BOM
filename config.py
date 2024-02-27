1ab15b94e704bfe96a838b7af21bf727cbf76d0c
GOODOOX <159203355+GOODOOX@users.noreply.github.com>
2024-02-27 04:18:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/z75z/Z07",
+    "https://github.com/GOODOOX/BO",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
